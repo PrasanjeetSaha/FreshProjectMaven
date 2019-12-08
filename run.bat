3db8232ca81f9e2e4208331724a8e9552d5350bc
Prasanjeet <prasanjeetsaha@gmail.com>
2019-12-08 18:44:15 +0530

    First commit for project setup...

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Prasanjeet\git\FreshStart\FreshProject
+mvn clean install
---
+++ src/test/java/FreshStart/FreshProject/Module1/Module1Objects.java
@@ -0,0 +1,10 @@
+package FreshStart.FreshProject.Module1;
+
+import org.testng.annotations.Test;
+
+public class Module1Objects {
+	@Test
+	public void firstMethod(){
+		System.out.println("This is my object class");
+	}
+}
